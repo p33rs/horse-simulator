8fbc50821cc3c6fc6afaf48b812df19b1e331ee0
p33rs <jon@peers.pw>
2015-01-04 04:45:21 -0500

    added views

---
+++ app/views/landing.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <title>
+        Horse Simulator 2015 (R) Home Premium Edition ## RESTRICTED ALPHA
+    </title>
+    <meta name="description" content="The world's premiere horse simulation experience.">
+    {{ HTML::script('js/vendor.js'); }}
+    {{ HTML::script('js/cram.js'); }}
+    {{ HTML::style('css/style.css'); }}
+</head>
+<body>
+    oh snap!
+</body>
+</html>
