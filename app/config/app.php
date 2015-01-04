a9404983e19302e8703317e4d147c291facfeb6f
p33rs <jon@peers.pw>
2015-01-03 22:06:24 -0500

    fix some boilerplate bugs

--- app/config/app.php
+++ app/config/app.php
@@ -121,6 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+        'horse\validators\ValidatorServiceProvider',
 
 	),
 
@@ -188,7 +189,6 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
-
 	),
 
 );
