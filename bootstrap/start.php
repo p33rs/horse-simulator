149e459a9dc20a295a2a2db0516c14b64732348f
Jon Pierce <jon@peers.pw>
2015-01-13 10:56:10 -0500

    adds hydrogen to dev hosts

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-    'local' => array('homestead', 'nitrogen', 'helium'),
+    'local' => array('homestead', 'nitrogen', 'helium', 'hydrogen'),
 
 ));
 
